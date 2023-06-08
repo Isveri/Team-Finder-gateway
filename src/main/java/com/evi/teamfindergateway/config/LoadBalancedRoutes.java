b9baeaeeb6202e971a6a46a6851c9d3a929e4ba1
Evistix <mephistakaiwan@gmail.com>
2023-06-08 20:40:58 +0200

    initial monolith split

--- src/main/java/com/evi/teamfindergateway/config/LoadBalancedRoutes.java
+++ src/main/java/com/evi/teamfindergateway/config/LoadBalancedRoutes.java
@@ -5,17 +5,41 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import static org.springframework.cloud.gateway.filter.factory.DedupeResponseHeaderGatewayFilterFactory.Strategy.RETAIN_FIRST;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS;
+import static org.springframework.http.HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN;
+
 @Configuration
 public class LoadBalancedRoutes {
 
     @Bean
     public RouteLocator customLoadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/api/v1/user", "/api/v1/user/*")
+                .route(r -> r.path("/api/v1/users", "/api/v1/users/*","api/v1/reports/*","api/v1/friends/*","api/v1/admin/*")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
                         .uri("lb://core-service")
                 )
                 .route(r -> r.path("/api/v1/auth", "/api/v1/auth/*")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
                         .uri("lb://auth-service")
+                )
+                .route(r -> r.path("/api/v1/notify/*", "/api/v1/notification/*")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
+                        .uri("lb://notification-service")
+                )
+                .route(r -> r.path("/api/v1/groups", "/api/v1/groups/*",
+                                "/api/v1/search","/api/v1/search/*",
+                                "/api/v1/userGroups","/api/v1/userGroups/*")
+                        .filters(f-> f
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
+                                .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
+                        .uri("lb://group-management-service")
                 )
                 .build();
     }
