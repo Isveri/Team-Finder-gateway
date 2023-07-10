ade9487ad1ae011af7fa004f347a418b266201aa
Evistix <mephistakaiwan@gmail.com>
2023-07-10 14:19:38 +0200

    refactoring

--- src/main/java/com/evi/teamfindergateway/config/LoadBalancedRoutes.java
+++ src/main/java/com/evi/teamfindergateway/config/LoadBalancedRoutes.java
@@ -15,37 +15,35 @@ public class LoadBalancedRoutes {
     @Bean
     public RouteLocator customLoadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route(r -> r.path("/api/v1/users", "/api/v1/users/**","/api/v1/reports/**","/api/v1/friends/**","/api/v1/admin/**")
-                        .filters(f-> f
+                .route(r -> r.path("/api/v1/users/**", "/api/v1/reports/**", "/api/v1/friends/**", "/api/v1/admin/**")
+                        .filters(f -> f
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
                         .uri("lb://core-service")
                 )
-                .route(r -> r.path("/api/v1/auth", "/api/v1/auth/**","/api/v1/platform/**")
-                        .filters(f-> f
+                .route(r -> r.path("/api/v1/auth/**", "/api/v1/platform/**")
+                        .filters(f -> f
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
                         .uri("lb://auth-service")
                 )
                 .route(r -> r.path("/api/v1/notify/**", "/api/v1/notification/**")
-                        .filters(f-> f
+                        .filters(f -> f
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
                         .uri("lb://notification-service")
                 )
-                .route(r -> r.path("/api/v1/groups", "/api/v1/groups/**",
-                                "/api/v1/search","/api/v1/search/**",
-                                "/api/v1/userGroups","/api/v1/userGroups/**")
-                        .filters(f-> f
+                .route(r -> r.path("/api/v1/groups/**",
+                                "/api/v1/search/**",
+                                "/api/v1/user-groups/**")
+                        .filters(f -> f
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
                         .uri("lb://group-management-service")
                 )
-                .route(r -> r.path("/api/v1/chat/**","/ws/**",
-                                "//chat/**","/privateChat/**","/api/v1/chatLogs/**",
-                                "/api/v1/users/chatLogs/*","/api/v1/deletedGroupsLogs/**","/api/v1/messageRead/**",
-                                "/api/v1/unreadMessages","/topic/**","/app/**")
-                        .filters(f-> f
+                .route(r -> r.path("/api/v1/chat/**", "/ws/**",
+                                "/chat/**", "/private/**", "/topic/**", "/app/**")
+                        .filters(f -> f
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_CREDENTIALS, RETAIN_FIRST.name())
                                 .dedupeResponseHeader(ACCESS_CONTROL_ALLOW_ORIGIN, RETAIN_FIRST.name()))
                         .uri("lb://chat-service")
